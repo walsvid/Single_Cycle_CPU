2ae704b253eca4fc322856f7507b3d1a0ff85a81
walsvid <walsvid@gmail.com>
2016-11-22 22:02:25 +0800

    walsvid - Add ADD32 module

---
+++ ADD32.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:56:13 11/22/2016 
+// Design Name: 
+// Module Name:    ADD32 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ADD32(
+	input [31:0] A,
+	input [31:0] B,
+	output [31:0] C
+    );
+
+	assign C = A + B;
+
+endmodule
