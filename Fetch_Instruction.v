c20ef655ac322cd304a9c6a1e309cbfd4de0403a
walsvid <walsvid@gmail.com>
2016-11-24 22:30:54 +0800

    walsvid - Add Fetch Instruction

---
+++ Fetch_Instruction.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:32:00 11/24/2016 
+// Design Name: 
+// Module Name:    Fetch_Instruction 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Fetch_Instruction(Branch, Jump, Clock, Reset, Zero, Inst, t1);
+	input Branch;
+	input Jump;
+	input Clock;
+	input Reset;
+	input Zero;
+	output [31:0]	Inst;
+	output [31:0] t1;
+	
+	wire [31:0] next_pc, addr, pc_4, imm32, imm32_4, pc_b, pc_s_b, pc_j;
+	// pc_4 : pc+4
+	// imm32_4 : imm32 * 4 or imm << 2
+	// pc_b : pc when branch instruction
+	// pc_s_b : select serial or branch pc
+	wire [25:0] target;
+	wire [15:0] imm16;
+	wire b_z;
+	
+	and(b_z, Branch, Zero);
+
+	PC pc_unit(next_pc, Clock, Reset, addr);
+	INST_MEM i_rom(addr, Inst);
+	
+	assign imm16 = Inst[15:0];
+	assign target = Inst[25:0];
+	
+	ADD32 adder_pc_4(32'h4, addr, pc_4);
+	
+	SIGN_EXT_16_32 ext_imm(imm16, imm32);
+	
+	SHIFTER shifter_imm(imm32, 5'h2, 1'b0, 1'b0, imm32_4);
+	
+	SHIFTER shifter_j({2'b00,{addr[31:28]},target}, 5'h2, 1'b0, 1'b0, pc_j);
+	
+	ADD32 adder_pc_4_imm32_4(pc_4, imm32_4, pc_b);
+	
+	
+	MUX_32_2_1 mux_branch(pc_4, pc_b, b_z, pc_s_b);
+	
+	MUX_32_2_1 mux_jump(pc_s_b, pc_j, Jump, next_pc);
+	
+	//assign next_pc = pc_s_b;
+	assign t1 = addr;
+endmodule
---
+++ Fetch_Instruction_tb.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:06:25 11/24/2016
+// Design Name:   Fetch_Instruction
+// Module Name:   C:/Users/wen/git/Single_Cycle_CPU/Fetch_Instruction_tb.v
+// Project Name:  Single_Cycle_CPU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Fetch_Instruction
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Fetch_Instruction_tb;
+
+	// Inputs
+	reg Branch;
+	reg Jump;
+	reg Clock;
+	reg Reset;
+	reg Zero;
+
+	// Outputs
+	wire [31:0] Inst;
+	wire [31:0] t1;
+
+	// Instantiate the Unit Under Test (UUT)
+	Fetch_Instruction uut (
+		.Branch(Branch), 
+		.Jump(Jump), 
+		.Clock(Clock), 
+		.Reset(Reset), 
+		.Zero(Zero), 
+		.Inst(Inst), 
+		.t1(t1)
+	);
+
+	initial begin
+		// Initialize Inputs
+		Branch = 0;
+		Jump = 1;
+		Clock = 0;
+		Reset = 1;
+		Zero = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+      Clock = ~Clock;
+		Reset = 0;
+		Jump = 0;
+		#100;
+      Clock = ~Clock;
+		#100;
+      Clock = ~Clock;
+		#100;
+      Clock = ~Clock;		
+		#100;
+      Clock = ~Clock;
+		#100;
+      Clock = ~Clock;
+		#100;
+      Clock = ~Clock;
+		#100;
+      Clock = ~Clock;
+		#100;
+      Clock = ~Clock;		
+		// Add stimulus here
+
+	end
+      
+endmodule
+
