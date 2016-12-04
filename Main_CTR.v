95b200a6f8885ba27493cb23db4ab575b4e5d9d1
walsvid <walsvid@gmail.com>
2016-12-04 20:53:16 +0800

    walsvid - Add bne in Main controller

--- Main_CTR.v
+++ Main_CTR.v
@@ -18,25 +18,27 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Main_CTR(op, RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch, Jump, Extop, ALUop, R_type);
+module Main_CTR(op, RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch_eq, Branch_ne, Jump, Extop, ALUop, R_type);
 	input [5:0] op;
 	output RegWr;
 	output ALUSrc;
 	output RegDst;
 	output MemtoReg;
 	output MemWr;
-	output Branch;
+	output Branch_eq;
+	output Branch_ne;
 	output Jump;
 	output Extop;
 	output R_type;
 	
 	output [3:0] ALUop;
 	
-	wire r_type, addi, andi, ori, xori, lw, sw, beq, lui, j ,jal;
+	wire r_type, addi, andi, ori, xori, lw, sw, beq, bne, lui, j ,jal;
 	
-	Instruction_Interpreter inst_inter(op, r_type, addi, andi, ori, xori, lw, sw, beq, lui, j ,jal);
+	Instruction_Interpreter inst_inter(op, r_type, addi, andi, ori, xori, lw, sw, beq, bne, lui, j ,jal);
 	
-	assign Branch = beq;
+	assign Branch_eq = beq;
+	assign Branch_ne = bne;
 	assign Jump = j | jal;
 	assign RegDst = r_type;
 	assign ALUSrc = addi | andi | ori | xori | lw | sw | beq | lui;
@@ -46,9 +48,9 @@ module Main_CTR(op, RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch, Jump, Extop,
 	assign Extop = lw | sw | beq;
 	assign R_type = r_type;
 	
-	assign ALUop[0] = r_type | ori | xori | lw | sw | beq;
+	assign ALUop[0] = r_type | ori | xori | lw | sw | beq | bne;
 	assign ALUop[1] = r_type | addi| andi | ori | xori | lui;
-	assign ALUop[2] = r_type | addi| xori | beq | lui;
+	assign ALUop[2] = r_type | addi| xori | beq | lui | bne;
 	assign ALUop[3] = r_type | addi;
 
 endmodule
