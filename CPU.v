1e9505e7c12e8b36172aa84be07101b9187ffaf1
walsvid <walsvid@gmail.com>
2016-12-11 18:28:34 +0800

    walsvid - Finish package CPU

--- CPU.v
+++ CPU.v
@@ -22,28 +22,45 @@ module CPU(Clock, Reset, busW, DataIn, ALU_Result);
 	input Clock;
 	input Reset;
 	input [31:0] busW;
-	output DataIn;
-	output ALU_Result;
-	
+	output [31:0] DataIn;
+	output [31:0] ALU_Result;
+//	
 	wire [4:0] Rd, Rt, Rs;
 	wire [15:0] imm16;
-	wire [31:0] Inst, ALU_B, ALU_A;
-	
-	Fetch_Instruction fetch(Clock, Reset, Zero, reg31, w_reg31, Inst);
+	wire [31:0] Inst, ALU_B, ALU_A, busA, busB, reg31, w_reg31, shamt;
+	wire Zero, Overflow;
+	wire RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch_eq, Branch_ne, Jump, Extop, Shift;
+	wire Write;
+	wire [3:0] ALUctr;
+	wire [31:0] imm32;
+	wire [5:0] func;
+	wire [5:0] op;
+
 	assign Rs = Inst[25:21];
 	assign Rt = Inst[20:16];
 	assign Rd = Inst[15:11];
+	assign op = Inst[31:26];
+	assign func = Inst[5:0];
+	assign shamt = {{27{0}},Inst[10:6]};
 	assign imm16 = Inst[15:0];
-	RegFile regiters(Ra, Rb, Rw, Clock, Write, busW, busA, busB, w_R31, r_R31);
-	
-	
-	ALU alu(A, B, ALUctr, Result, Zero, Overflow);
-	Control_Unit controller(op, func, ALUctr, RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch_eq, Branch_ne, Jump, Extop);
-	
-	MUX_5_2_1 mux_regdst(Rd, Rt, RegDst, Rw);
 
-	MUX_32_2_1 mux_a(A,B,Sel,o);
-	MUX_32_2_1 mux_b(A,B,Sel,o);
+	Fetch_Instruction fetch(Clock, Reset, Zero, Branch_eq, Branch_ne, Jump, reg31, w_reg31, Inst);
+	and(Write, Overflow, RegWr);
+	MUX_5_2_1 mux_regdst(Rd, Rt, RegDst, Rw);
+	RegFile regiters(Rs, Rt, Rw, Clock, Write, busW, busA, busB, w_reg31, reg31);
+   Control_Unit controller(op, func, ALUctr, RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch_eq, Branch_ne, Jump, Extop, Shift);
+	ALU myalu(ALU_A, ALU_B, ALUctr, ALU_Result, Zero, Overflow);
+
+
 	
+   MUX_32_2_1 mux_a(busA, shamt, Shift, ALU_A);
+	EXT_16_32 immext(Extop, imm16, imm32);
+	MUX_32_2_1 mux_b(busB, imm32, ALUSrc, ALU_B);
+
+	
+	
+
+	
+	assign DataIn = busB;
 
 endmodule
