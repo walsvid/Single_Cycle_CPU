241230bb2fc57612755896290e39a29727f8dbf6
walsvid <walsvid@gmail.com>
2016-12-11 22:21:08 +0800

    walsvid - Add CPU test

--- CPU.v
+++ CPU.v
@@ -18,16 +18,17 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module CPU(Clock, Reset, busW, DataIn, ALU_Result);
+module CPU(Clock, Reset, tmp);
 	input Clock;
 	input Reset;
-	input [31:0] busW;
-	output [31:0] DataIn;
-	output [31:0] ALU_Result;
-//	
-	wire [4:0] Rd, Rt, Rs;
+//	input [31:0] busW;
+//	output [31:0] DataIn;
+//	output [31:0] ALU_Result;
+	output [31:0] tmp;
+
+	wire [4:0] Rd, Rt, Rs, Rw;
 	wire [15:0] imm16;
-	wire [31:0] Inst, ALU_B, ALU_A, busA, busB, reg31, w_reg31, shamt;
+	wire [31:0] Inst, ALU_B, ALU_A, busA, busB, busW, reg31, w_reg31, shamt, ALU_Result;
 	wire Zero, Overflow;
 	wire RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch_eq, Branch_ne, Jump, Extop, Shift;
 	wire Write;
@@ -35,6 +36,11 @@ module CPU(Clock, Reset, busW, DataIn, ALU_Result);
 	wire [31:0] imm32;
 	wire [5:0] func;
 	wire [5:0] op;
+	
+	wire [31:0] dataout;
+	
+	//tmp
+	assign tmp = Inst;
 
 	assign Rs = Inst[25:21];
 	assign Rt = Inst[20:16];
@@ -45,22 +51,22 @@ module CPU(Clock, Reset, busW, DataIn, ALU_Result);
 	assign imm16 = Inst[15:0];
 
 	Fetch_Instruction fetch(Clock, Reset, Zero, Branch_eq, Branch_ne, Jump, reg31, w_reg31, Inst);
-	and(Write, Overflow, RegWr);
+	and(Write, ~Overflow, RegWr);
 	MUX_5_2_1 mux_regdst(Rd, Rt, RegDst, Rw);
 	RegFile regiters(Rs, Rt, Rw, Clock, Write, busW, busA, busB, w_reg31, reg31);
    Control_Unit controller(op, func, ALUctr, RegWr, ALUSrc, RegDst, MemtoReg, MemWr, Branch_eq, Branch_ne, Jump, Extop, Shift);
 	ALU myalu(ALU_A, ALU_B, ALUctr, ALU_Result, Zero, Overflow);
-
 
 	
    MUX_32_2_1 mux_a(busA, shamt, Shift, ALU_A);
 	EXT_16_32 immext(Extop, imm16, imm32);
 	MUX_32_2_1 mux_b(busB, imm32, ALUSrc, ALU_B);
 
-	
-	
 
-	
 	assign DataIn = busB;
+	
+	DATA_MEM d_rom(dataout, busB, ALU_Result, MemWr, Clock);
+	
+	MUX_32_2_1 mux_mem_to_reg(busA, dataout, MemtoReg, busW);
 
 endmodule
---
+++ CPU_tb.v
@@ -0,0 +1,97 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   00:27:20 01/01/2008
+// Design Name:   CPU
+// Module Name:   D:/Single_Cycle_CPU/CPU_tb.v
+// Project Name:  Single_Cycle_CPU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: CPU
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module CPU_tb;
+
+	// Inputs
+	reg Clock;
+	reg Reset;
+
+	// Outputs
+	wire [31:0] tmp;
+
+	// Instantiate the Unit Under Test (UUT)
+	CPU uut (
+		.Clock(Clock), 
+		.Reset(Reset),
+		.tmp(tmp)
+	);
+
+	initial begin
+		// Initialize Inputs
+		Clock = 0;
+		Reset = 1;
+
+		// Wait 100 ns for global reset to finish
+		#50;
+      Clock = ~Clock;
+		Reset = 0;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;
+		#50;
+      Clock = ~Clock;		
+		// Add stimulus here
+
+	end
+      
+endmodule
+
